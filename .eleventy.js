87689b33b2cc69573f4bb5ca1ca67522b1589830
Tom Lynch <tomglynch@gmail.com>
2021-04-13 23:53:03 +1000

    s

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ const { DateTime } = require("luxon");
 
 function sortByOrder(values) {
     let vals = [...values];     // this *seems* to prevent collection mutation...
-    console.log(vals)
     return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
 }
 
